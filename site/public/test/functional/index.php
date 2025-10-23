e8a06ae2a5a2dba0e72b2f337e939e7da147638c
Ivan-profan <61698771+ivan-profun@users.noreply.github.com>
2025-10-23 21:17:58 +0300

    added: page styling and structure

--- site/public/test/functional/index.php
+++ site/public/test/functional/index.php
@@ -4,49 +4,67 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Cтраница теста функционала</title>
+    <link rel="stylesheet" href="/styles/global.css">
     <link rel="stylesheet" href="/styles/test.css">
 </head>
 <body>
 	<header>
-		<nav class="nav-container">
-			<ul class="nav-list">
-				<li class="nav-item"><a href="/test/" class="nav-link">Главная</a></li>
-				<li class="nav-item"><a href="/test/functional/" class="nav-link">Функционал</a></li>
-			</ul>
-		</nav>
+		<div class="top">
+			<nav class="nav-container">
+				<ul class="nav-list">
+					<li class="nav-item"><a href="/test/" class="nav-link">Главная</a></li>
+					<li class="nav-item"><a href="/test/functional/" class="nav-link">Функционал</a></li>
+				</ul>
+			</nav>
+		</div>
 	</header>
 	<main>
 		<div class="head">
 			<h1 class="title">Страница теста функционала</h1>
 			<div class="desc">Здесь находятся ссылки на тесты функционала</div>
 		</div>
-		<div class="nav-conteiner tests">
-			<ul class="nav-list">
-				<li class="nav-item">
-					<a href="./test_cookie.php" class="nav-link">
+		<div class="tests">
+			<div class="list">
+				<div class="item">
+					<a href="./test_cookie.php" class="item-link">
 						<div class="head">Cookie</div>
 						<div class="desc">Тестирование записи данных в куки</div>
 					</a>
-				</li>
-			</ul>
+				</div>
+			</div>
 		</div>
 	</main>
 	<footer>
 		<div class="top">
+			<div class="logo-block">
+				<div class="logo">
+					<a href="/">
+						<div class="logo-text">
+							Тут могло быть<br>
+							моё лого
+						</div>
+					</a>
+				</div>
+			</div>
 			<nav class="nav-container">
 				<ul class="nav-list">
-					<li class="nav-item"><a class="nav-link">Навигация по сайту</a></li>
+					<li class="nav-item"><a class="nav-title">Навигация по сайту</a></li>
 					<li class="nav-item"><a href="/" class="nav-link">Главная</a></li>
 				</ul>
 				<ul class="nav-list">
-					<li class="nav-item"><a class="nav-link">Навигация по тестам</a></li>
+					<li class="nav-item"><a class="nav-title">Навигация по тестам</a></li>
 					<li class="nav-item"><a href="/test/" class="nav-link">Главная</a></li>
 					<li class="nav-item"><a href="/test/functional/" class="nav-link">Функционал</a></li>
 				</ul>
 			</nav>
 		</div>
 		<div class="bottom">
-			
+			<div class="copyright">
+				Made in Russian
+			</div>
+			<div class="sitemap">
+				Карты сайта пока нет!
+			</div>
 		</div>
 	</footer>
 </body>
